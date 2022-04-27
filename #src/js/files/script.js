eafa25b08b4dde7467f608fff7be24ee9ea760fd
[hkiper] <semin.ili14@gmail.com>
2022-04-27 12:34:01 +0500

    complete 100%

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -4,3 +4,22 @@ linkLi.addEventListener("click", function(){
 		subLink.classList.toggle('_active');
 		linkLi.classList.toggle("_open");	
  });
+
+
+
+
+ 
+ window.onscroll = function() {scrollFunction()};
+ function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		 document.getElementById("myBtn").style.display = "block";
+	} else {
+		 document.getElementById("myBtn").style.display = "none";
+	}
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
--- electric/js/app.js
+++ electric/js/app.js
@@ -796,6 +796,25 @@ linkLi.addEventListener("click", function(){
 		subLink.classList.toggle('_active');
 		linkLi.classList.toggle("_open");	
  });
+
+
+
+
+ 
+ window.onscroll = function() {scrollFunction()};
+ function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		 document.getElementById("myBtn").style.display = "block";
+	} else {
+		 document.getElementById("myBtn").style.display = "none";
+	}
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
 //let btn = document.querySelectorAll('button[type="submit"],input[type="submit"]');
 let forms = document.querySelectorAll('form');
 if (forms.length > 0) {
